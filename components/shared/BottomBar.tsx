d1aeb5a3d173b87aa9ef8d2ad24e40caf821ec68
AntonKh <leaterood@gmail.com>
2023-08-17 14:03:39 +0300

    top, left, bottom bars

--- components/shared/BottomBar.tsx
+++ components/shared/BottomBar.tsx
@@ -1,5 +1,44 @@
+"use client";
+
+import { sidebarLinks } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
 function BottomBar() {
-  return <h1>BottomBar</h1>;
+  const pathname = usePathname();
+
+  return (
+    <section className="bottombar">
+      <div className="bottombar_container">
+        {sidebarLinks.map((link) => {
+          // Для того что бы отслеживать какая из ссылок сейчас активная сделали явный ретерн
+          // Для того что бы определить активную ссылку надо использовать хуки usePathname, useRouter из библиотеки next/navigation, сможем узнать актуальный адрес URL
+          const isActive =
+            (pathname.includes(link.route) && link.route.length > 1) ||
+            pathname === link.route;
+          return (
+            <Link
+              href={link.route}
+              key={link.label}
+              className={`bottombar_link ${isActive && "bg-primary-500"}`}
+            >
+              <Image
+                src={link.imgURL}
+                alt={link.label}
+                width={24}
+                height={24}
+              />
+              <p className="text-subtle-medium text-light-1 max-sm:hidden">
+                {link.label.split(/\s+/)[0]}
+                {/* так как create thread занимает две строки и в нижнем баре все съезжает использовал регулярное выраждение которое разделяет строку на отдельные подстроки через пробел и обращается только к первому элементу, то есть к слову create. получается что тогда все элементы нижнего меню будут одинакого размера. */}
+              </p>
+            </Link>
+          );
+        })}
+      </div>
+    </section>
+  );
 }
 
 export default BottomBar;
--- components/shared/LeftSideBar.tsx
+++ components/shared/LeftSideBar.tsx
@@ -1,5 +1,59 @@
+//! так как хук useRouter и usePathname работает только с компонентами на стороне клиента надо указать что этот файл будет работать именно там, если не указать то будет ошибка
+"use client";
+
+import { sidebarLinks } from "@/constants";
+import { SignedIn, SignOutButton } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
 function LeftSideBar() {
-  return <h1>LeftSideBar</h1>;
+  const router = useRouter();
+  const pathname = usePathname();
+
+  return (
+    <section className="custom-scrollbar leftsidebar">
+      <div className="flex w-full flex-1 flex-col gap-6 px-6">
+        {sidebarLinks.map((link) => {
+          // Для того что бы отслеживать какая из ссылок сейчас активная сделали явный ретерн
+          // Для того что бы определить активную ссылку надо использовать хуки usePathname, useRouter из библиотеки next/navigation, сможем узнать актуальный адрес URL
+          const isActive =
+            (pathname.includes(link.route) && link.route.length > 1) ||
+            pathname === link.route;
+          return (
+            <Link
+              href={link.route}
+              key={link.label}
+              className={`leftsidebar_link ${isActive && "bg-primary-500"}`}
+            >
+              <Image
+                src={link.imgURL}
+                alt={link.label}
+                width={24}
+                height={24}
+              />
+              <p className="text-light-1 max-lg:hidden">{link.label}</p>
+            </Link>
+          );
+        })}
+      </div>
+      <div className="mt-10 px-6">
+        <SignedIn>
+          <SignOutButton signOutCallback={() => router.push("/sign-in")}>
+            {/* signOutCallback это коллбек который будет вызван после того как пользователь успешно разлогиниться, в этом случае если юзер разлогиниться его сразу перенаправит на страницу с регистрацией */}
+            <div className="flex cursor-pointer gap-4 px-4">
+              <Image
+                src="/assets/logout.svg"
+                alt="logout"
+                width={24}
+                height={24}
+              />
+              <p className="text-light-2 max-lg:hidden">Logout</p>
+            </div>
+          </SignOutButton>
+        </SignedIn>
+      </div>
+    </section>
+  );
 }
 
 export default LeftSideBar;
